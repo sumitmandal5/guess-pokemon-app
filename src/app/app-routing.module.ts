a7ca138553c56f0e74233ad05df55d369dfa979e
sumitmandal5 <sumit.kumar03@gmail.com>
2025-03-01 15:41:30 +0000

    Added guess component and navigating to it programatically.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PokemonGuessComponent } from './components/pokemon-guess/pokemon-guess.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'guess', component: PokemonGuessComponent }
 ];
 
 @NgModule({
---
+++ src/app/components/pokemon-guess/pokemon-guess.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-guess',
+  templateUrl: './pokemon-guess.component.html',
+  styleUrls: ['./pokemon-guess.component.scss']
+})
+export class PokemonGuessComponent {
+
+}
