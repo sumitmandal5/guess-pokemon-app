797edfb4d794d1aee6be1ad41f0ad2294c16fc07
sumitmandal5 <sumit.kumar03@gmail.com>
2025-03-01 16:18:37 +0000

    Service added. fetch random pokemon working.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PokemonGuessComponent } from './components/pokemon-guess/pokemon-guess.component';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -9,7 +10,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/pokemon-guess/pokemon-guess.component.ts
+++ src/app/components/pokemon-guess/pokemon-guess.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-guess',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-guess.component.scss']
 })
 export class PokemonGuessComponent {
+  pokemonId!: number;
+  silhouetteImage!: string;
+  options: string[] = [];
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.fetchRandomPokemon();
+  }
+
+  fetchRandomPokemon(): void {
+    this.pokemonService.getRandomPokemon().subscribe({
+      next: (data) => {
+        this.pokemonId = data.id;
+        this.silhouetteImage = data.silhouetteImage;
+        this.options = data.options;
+      },
+      error: (err) => {
+        console.error('Error fetching Pokemon:', err);
+      }
+    });
+  }
 
 }
